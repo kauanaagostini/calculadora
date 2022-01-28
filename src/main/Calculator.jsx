a9e6cf7f93849a12f6255ca4bd7bfc4cf6a66704
Kauana Maria Agostini <kauanaagostini@gmail.com>
2022-01-28 16:27:52 -0300

    criado componente principal da calculadora.

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  return (
+    <div className="calculator">
+      
+    </div>
+  )
+}
+
+export default Calculator
